99973d7116ede4133c9e73979475b507b2443c4b
Daniil Videneev <danyavid@gmail.com>
2018-05-04 19:47:41 +0300

    Change args type

--- data/resolvers.js
+++ data/resolvers.js
@@ -4,39 +4,33 @@ import { Cashier } from './mongoose-connector';
 
 const resolvers = {
   Query: {
-    author(root, args) {
-      return { id: 1, firstName: 'Hello', lastName: 'World' };
-    },
-    allAuthors() {
-      return [{ id: 1, firstName: 'Hello', lastName: 'World' }];
-    },
     cashier(id) {
       return Cashier.findById(id)
     },
-    composition(root, args) {
+    composition(root, { id }) {
       return neo4j.driver.session().run(
         'MATCH (c:Composition {id: {id}}) RETURN c',
-        {id: args.id}
+        {id: id}
       ).then(result => {
         return neo4j.recordToComposition(result.records[0]);
       }).catch(error => {
         console.log("composition result error: " + error);
       })
     },
-    row(root, args) {
+    row(root, { id }) {
       return neo4j.driver.session().run(
         'MATCH (r:Row {id: {id}}) RETURN r',
-        {id: args.id}
+        {id: id}
       ).then(result => {
         return neo4j.recordToRow(result.records[0]);
       }).catch(error => {
         console.log("row result error: " + error);
       })
     },
-    shelfDish(root, args) {
+    shelfDish(root, { id }) {
       return neo4j.driver.session().run(
         'MATCH (d:Dish {id: {id}}) RETURN d',
-        {id: args.id}
+        {id: id}
       ).then(result => {
         return result.records.map(neo4j.recordToShelfDish)
       }).catch(error => {
@@ -51,19 +45,6 @@ const resolvers = {
       }).catch(error => {
         console.log("All dishes result error: " + error);
       })
-    }
-  },
-  Author: {
-    posts(author) {
-      return [
-        { id: 1, title: 'A post', text: 'Some text', views: 2 },
-        { id: 2, title: 'Another post', text: 'Some other text', views: 200 }
-      ];
-    }
-  },
-  Post: {
-    author(post) {
-      return { id: 1, firstName: 'Hello', lastName: 'World' };
     }
   },
   Row: {
--- data/schema.js
+++ data/schema.js
@@ -1,24 +1,14 @@
-import { makeExecutableSchema } from 'graphql-tools';
 import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
 import resolvers from './resolvers';
 
 const typeDefs = `
 type Query {
   cashier(id: ID): Cashier
-  author(firstName: String, lastName: String): Author
-  allAuthors: [Author]
   getFortuneCookie: String # we'll use this later
   composition(id: Int!): Composition
   row(id: Int!): Row
   shelfDish(id: Int!): [ShelfDish]
   allShelfDishes: [ShelfDish]
-}
-
-type Author {
-  id: Int
-  firstName: String
-  lastName: String
-  posts: [Post]
 }
 
 type Cashier {
